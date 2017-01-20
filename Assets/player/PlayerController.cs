5fb29d70b986cb5e0ceeef5cb5ec07579a263681
Bart Huylebroeck <BahuMan@users.noreply.github.com>
2017-01-20 20:01:21 +0100

    rough sketch + jump animation

---
+++ Assets/player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    public float jumpForce = 15f;
+    private Rigidbody2D thisRigid;
+
+	// Use this for initialization
+	void Start () {
+        thisRigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            thisRigid.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            Debug.Log("jump!");
+        }
+	}
+}
