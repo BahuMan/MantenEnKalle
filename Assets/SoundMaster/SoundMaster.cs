fef718d300bf8db4c2f53570d33d89e13cf088fd
Bart Huylebroeck <BahuMan@users.noreply.github.com>
2017-01-21 00:20:22 +0100

    resolve merge rommel in code

--- Assets/SoundMaster/SoundMaster.cs
+++ Assets/SoundMaster/SoundMaster.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundMaster : MonoBehaviour {
-<<<<<<< HEAD
 	private float nextActionTimer;
 	public bool enemy1_ready;
 	public bool enemy2_ready;
@@ -16,35 +15,28 @@ public class SoundMaster : MonoBehaviour {
 	//public AudioClip[] vp;
 
 	public EventNot parseStart;
-=======
->>>>>>> 6ed384d55d6ee839c42cad01ef25dd110e8c1460
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-<<<<<<< HEAD
-		if (Time.time >= nextActionTimer) {
-			//parseStart.measureStarted ();
-			if (enemy1_ready) {
-				int soundSelector = Random.Range(0, vl.Length - 1); 
-				source.PlayOneShot (vl[soundSelector]);
-			}
-			nextActionTimer = Time.time + 4;
-			int soundSelectorS = Random.Range(0, vl.Length); 
-			source.PlayOneShot (vl[soundSelectorS]);
-			Debug.Log (soundSelectorS);
-	    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextActionTimer)
+        {
+            //parseStart.measureStarted ();
+            if (enemy1_ready)
+            {
+                int soundSelector = Random.Range(0, vl.Length - 1);
+                source.PlayOneShot(vl[soundSelector]);
+            }
+            nextActionTimer = Time.time + 4;
+            int soundSelectorS = Random.Range(0, vl.Length);
+            source.PlayOneShot(vl[soundSelectorS]);
+            Debug.Log(soundSelectorS);
+        }
+    }
 
 }
-
-}
-=======
-		
-	}
-}
->>>>>>> 6ed384d55d6ee839c42cad01ef25dd110e8c1460
