b98a6d92ff889b4e851afd2c2c9373b73a4b453f
jeroenpx <jeroenpx@users.noreply.github.com>
2017-01-21 17:43:27 +0100

    Changing scale of scene

---
+++ Assets/waves/utils/MeshSortingLayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSortingLayer : MonoBehaviour {
+
+	public string sortingLayer;
+	public int orderInLayer;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().sortingLayerName = sortingLayer;
+		GetComponent<Renderer> ().sortingOrder = orderInLayer;
+	}
+}
