ed3492a7a25abcc9e16ab5ace0c3823521f8825b
jeroenpx <jeroenpx@users.noreply.github.com>
2017-01-21 16:47:51 +0100

    Wave bar + configurable wave form

--- Assets/waves/wave/WaveFormRender.cs
+++ Assets/waves/wave/WaveFormRender.cs
@@ -10,6 +10,12 @@ public class WaveFormRender : BaseWaveFormRenderer {
 	public float frequency = 1f;
 	public float speed = -2f;
 
+	public float waveheight = 1f;
+
+	public float power = 4f;
+
+	public AnimationCurve wavemultiplier = new AnimationCurve();
+
 	// Use this for initialization
 	void Start () {
 		InitMesh ();
@@ -18,9 +24,18 @@ public class WaveFormRender : BaseWaveFormRenderer {
 	// Update is called once per frame
 	void Update () {
 		UpdateWave (delegate(float t) {
-			float multiplier = Mathf.Sin (t * Mathf.PI);
+			float multiplier = wavemultiplier.Evaluate(t);
+			if(multiplier<0) {
+				multiplier = 0;
+			}
 
-			float y = Mathf.Sin ((t+speed*Time.timeSinceLevelLoad)*Mathf.PI*2*frequency)*multiplier;
+			float y = Mathf.Sin ((t+speed*Time.timeSinceLevelLoad)*Mathf.PI*2*frequency+Mathf.PI/2);
+			if(y<0) {
+				y = 0;
+			} else {
+				y=Mathf.Pow(y, power);
+			}
+			y = y*multiplier*waveheight;
 
 			float ytop = lineheight/2f;
 			float ybottom = -lineheight/2f;
---
+++ Assets/waves/wavebars/WaveBarDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveBarDisplay : MonoBehaviour {
+
+	public List<float> waveFreq;
+
+	public float heightOfBars = 3f;
+	public float percentUsed = 0.8f;
+	public float maatWidth = 2f;
+
+	public GameObject wavePrefab;
+
+	private GameObject[] waves;
+	private WaveFormRender[] waverenderers;
+
+	// Use this for initialization
+	void Start () {
+		waves = new GameObject[waveFreq.Count];
+		waverenderers = new WaveFormRender[waveFreq.Count];
+		for (int i = 0; i < waveFreq.Count; i++) {
+			waves[i] = GameObject.Instantiate <GameObject> (wavePrefab, transform);
+			waverenderers [i] = waves [i].GetComponent<WaveFormRender> ();
+
+			waverenderers [i].waveheight = heightOfBars * percentUsed / 2f / waveFreq.Count;
+			waverenderers [i].frequency = waverenderers [i].width/maatWidth * waveFreq [i];
+			Transform waveTransform = waves [i].transform;
+			waveTransform.localPosition = new Vector3(0, (i+0.5f) * heightOfBars / waveFreq.Count-heightOfBars, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
