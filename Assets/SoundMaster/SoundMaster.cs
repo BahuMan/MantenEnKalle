737effcd592c8ff6b9fabd0a99a4bcf2675294ee
Ischa <Soeteweyischa@gmail.com>
2017-01-21 22:56:26 +0100

    Sound drift fixed

--- Assets/SoundMaster/SoundMaster.cs
+++ Assets/SoundMaster/SoundMaster.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundMaster : MonoBehaviour {
-    const float BEAT_DURATION = 4;
-    const float MAIN_DURATION = 3*BEAT_DURATION;
+    const float BEAT_DURATION = 2;
+    const float MAIN_DURATION = 24;
 
 	private float nextActionTimer = 0;
     private float nextMainTimer = 0;
@@ -31,26 +31,29 @@ public class SoundMaster : MonoBehaviour {
         if (Time.time >= nextActionTimer)
         {
             //parseStart.measureStarted ();
-
+			Debug.Log ("nextAction =" + nextActionTimer);
 
 			if (controller.GetNrEnemiesForFrequency(8) > 0)
             {
+				Debug.Log ("startInstrument8");
                 int soundSelector = Random.Range(0, vl.Length - 1);
                 source.PlayOneShot(vl[soundSelector]);
             }
 
 			if (controller.GetNrEnemiesForFrequency(2) > 0)
 			{
+				Debug.Log ("startInstrument2");
 				source.PlayOneShot (bd);
 			}
 				
-            nextActionTimer = Time.time + BEAT_DURATION;
+			nextActionTimer = nextActionTimer + BEAT_DURATION;
         }
 
         if (Time.time >= nextMainTimer)
         {
+			Debug.Log ("StartingMain");
             source.PlayOneShot(mainTheme);
-            nextMainTimer = Time.time + MAIN_DURATION;
+			nextMainTimer = nextMainTimer + MAIN_DURATION;
         }
     }
 
