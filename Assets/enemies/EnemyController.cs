51fc4a175dc9eb047052df43a3ef0259ca637aec
Bart Huylebroeck <BahuMan@users.noreply.github.com>
2017-01-21 13:34:43 +0100

    added critter, more blasting

--- Assets/enemies/EnemyController.cs
+++ Assets/enemies/EnemyController.cs
@@ -6,15 +6,17 @@ public class EnemyController : MonoBehaviour {
 
 	public int frequency;
 	public float speed;
+    private Rigidbody2D thisRigid;
 
 	// Use this for initialization
 	void Start () {
-		
+        thisRigid = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = transform.position + transform.right * -speed * Time.deltaTime;
+        //transform.position = transform.position + transform.right * -speed * Time.deltaTime;
+        thisRigid.AddForce(transform.right * -speed);
 	}
 
     public int getFrequency()
--- Assets/player/PlayerControllerBlast.cs
+++ Assets/player/PlayerControllerBlast.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class PlayerController : MonoBehaviour, EventNot {
+public class PlayerControllerBlast : MonoBehaviour, EventNot {
 
     public GameObject blastPrefab;
     public Collider2D ground;
